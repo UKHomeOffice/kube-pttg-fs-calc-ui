16478028fc58a9dc2d69d4d2cd508c5140019575
Stuart Craigen <s.craigen@mac.com>
2018-03-13 11:17:47 +0000

    ACP-migration

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,49 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
-export ENVIRONMENT=${ENVIRONMENT}
-export APP=pttg-fs-calc-ui
-export KUBE_SERVER=${KUBE_SERVER_DEV}
-export KUBE_TOKEN=${KUBE_TOKEN}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
+fi
+
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_FS_PR drone secret"
+    export KUBE_TOKEN=${PTTG_FS_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_FS_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_FS_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_FS_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_FS_DEV}
+    fi
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
+#export WHITELIST=${WHITELIST:-0.0.0.0/0}
+export WHITELIST=0.0.0.0/0
+
+if [ "${ENVIRONMENT}" == "pr" ] ; then
+    export DNS_PREFIX=
+    export KC_REALM=pttg-production
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+    export KC_REALM=pttg-qa
+fi
+
+export DOMAIN_NAME=fs.calc.${DNS_PREFIX}pttg.homeoffice.gov.uk
+
+echo "DOMAIN_NAME is $DOMAIN_NAME"
 
 cd kd
-kd --debug \
-   --insecure-skip-tls-verify --timeout 5m0s \
-   --file pttg-fs-calc-ui-deployment.yaml \
-   --file pttg-fs-calc-ui-ingress.yaml \
-   --file pttg-fs-calc-ui-svc.yaml
+
+kd --insecure-skip-tls-verify \
+    -f networkPolicy.yaml \
+    -f ingress.yaml \
+    -f deployment.yaml \
+    -f service.yaml
